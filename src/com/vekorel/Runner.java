dbd9107128927f59e62d54f665c0f0f6e81edab6
vektoreljavaali <vektoreljavaali@gmail.com>
2022-02-06 13:15:39 +0300

    Initial commit

---
+++ src/com/vekorel/Runner.java
@@ -0,0 +1,18 @@
+package com.vekorel;
+
+public class Runner {
+    public static void main(String[] args) {
+        /**
+         * Bir firma ürünlerinin tutacak, 3 grup ürünü var.
+         * BireyselUrun, KurumsalUrun, OzelYapimUrun
+         * id,ad,marka,model,fiyat,stokmiktari,uyarimiktari alanları ortaktır.
+         * Tüm ürünler için Tek bir SatisRepository sınıfı vardır. burada
+         * ürüne göre satış yapılıyor.
+         * Yukarıda ki bilgilere göre,
+         * 1- Kalıtım yapınız.
+         * 2- Her sınıf ta, default ve parametreli constructorlar yazınız.
+         * 3- Bazı özelliklerin ve methodların public,private ve protected olması gerekli
+         * 4- Runner da nesne oluturup, Repository sınıfında satışını yapınız.
+         */
+    }
+}
